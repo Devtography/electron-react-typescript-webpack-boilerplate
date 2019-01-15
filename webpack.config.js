f8ac31ae26773cffb222fb041b54c350faabcd13
Wing Chau <wing@devtography.com>
2019-01-16 06:22:28 +0800

    Refactor Webpack Config
    - Extracted the common settings from `mainConfig` & `rendererConfig`.
    - Installed package `lodash` to deep clone an object.

--- webpack.config.js
+++ webpack.config.js
@@ -1,101 +1,81 @@
+const lodash = require('lodash');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-function srcPath(src) {
+function joinPaths(src) {
   return path.join(__dirname, src);
 }
 
-let mainConfig = {
+// #region Common settings
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+const node = { __dirname: false, __filename: false };
+
+const resolve = {
+  alias: {
+    '~': __dirname,
+    main: joinPaths('src/main'),
+    models: joinPaths('src/models'),
+    public: joinPaths('public'),
+    renderer: joinPaths('src/renderer'),
+    utils: joinPaths('src/utils'),
+  },
+  extensions: ['.css', '.scss', '.js', '.json', '.ts', '.tsx'],
+};
+
+const commonModules = {
+  rules: [
+    {
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      loader: 'ts-loader',
+    },
+    {
+      test: /\.(jpg|png|svg|ico)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[path][name].[ext]',
+      },
+    },
+  ],
+};
+// #endregion
+
+const mainConfig = {
   entry: './src/main/main.ts',
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode,
   target: 'electron-main',
   output: {
     filename: 'main.bundle.js',
-    path: __dirname + '/dist',
+    path: joinPaths('dist'),
   },
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-  resolve: {
-    alias: {
-      '~': __dirname,
-      'actions': srcPath('src/actions'),
-      'main': srcPath('src/main'),
-      'models': srcPath('src/models'),
-      'reducers': srcPath('src/reducers'),
-      'utils': srcPath('src/utils')
-    },
-    extensions: ['.js', '.json', '.ts'],
-  },
-  module: {
-    rules: [
-      {
-        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-        test: /\.(ts)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-        },
-      },
-      {
-        test: /\.(jpg|png|svg|ico)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      },
-    ],
-  },
+  node,
+  resolve,
+  module: commonModules,
 };
 
-let rendererConfig = {
+const rendererConfig = {
   entry: './src/renderer/renderer.tsx',
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode,
   target: 'electron-renderer',
   output: {
     filename: 'renderer.bundle.js',
-    path: __dirname + '/dist',
+    path: joinPaths('dist'),
   },
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-  resolve: {
-    alias: {
-      '~': __dirname,
-      'public': srcPath('public'),
-      'models': srcPath('src/models'),
-      'renderer': srcPath('src/renderer'),
-      'utils': srcPath('src/utils')
-    },
-    extensions: ['.js', '.json', '.ts', '.tsx'],
-  },
-  module: {
-    rules: [
-      {
-        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-        },
-      },
+  node,
+  resolve,
+  module: (function () { 
+    const rendererModules = lodash.cloneDeep(commonModules);
+    rendererModules.rules.push(
       {
         test: /\.(scss|css)$/,
-        use: [
-          'css-loader?sourceMap',
-        ],
-      },
-      {
-        test: /\.(jpg|png|svg|ico)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      },
-    ],
-  },
+        use: ['css-loader'],
+      }
+    );
+
+    return rendererModules;
+  })(),
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './public/index.html'),
