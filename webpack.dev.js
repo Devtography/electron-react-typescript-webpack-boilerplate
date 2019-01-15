25f8cfee6c75e594f7ab7b01dcbbb79d8b3803ac
Wing Chau <wing@devtography.com>
2019-01-16 02:18:34 +0800

    Update Indent Size

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,9 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.common.js');
 
 module.exports = [
-    merge(baseConfig[0], {
-        mode: 'development'
-    }),
-    merge(baseConfig[1], {
-        mode: 'development'
-    })];
+  merge(baseConfig[0], {
+    mode: 'development'
+  }),
+  merge(baseConfig[1], {
+    mode: 'development'
+  })];
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,9 +2,9 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.common.js');
 
 module.exports = [
-    merge(baseConfig[0], {
-        mode: 'production'
-    }),
-    merge(baseConfig[1], {
-        mode: 'production'
-    })];
+  merge(baseConfig[0], {
+    mode: 'production'
+  }),
+  merge(baseConfig[1], {
+    mode: 'production'
+  })];
