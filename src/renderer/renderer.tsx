e6d0bfd9f33d95b4c892676bc04f8404a1486c11
Wing Chau <wing@devtography.com>
2020-12-26 10:53:27 +0000

    Fix ESLint warnings in `renderer`

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,16 +1,16 @@
 /**
  * React renderer.
  */
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
 // Import the styles here to process them with webpack
 import '@public/style.css';
 
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
 ReactDOM.render(
-  <div className='app'>
+  <div className="app">
     <h4>Welcome to React, Electron and Typescript</h4>
     <p>Hello</p>
   </div>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
