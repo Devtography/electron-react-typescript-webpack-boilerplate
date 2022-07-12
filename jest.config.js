6bc8b642f8b9885f2fa236a79cb7e265ece8fd9a
Wing Chau <wing@devtography.com>
2022-07-12 23:55:33 +0100

    Config Jest
    - Added Jest config file.
    - Set ESLint to ignore Jest config file.
    - Config TypeScript to not treat files in `test` folder as part of the source code.

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.json')
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = config = {
+  testEnvironment: 'node',
+  globals: {
+    'ts-test': {
+      tsconfig: 'tsconfig.json'
+    }
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleNameMapper: pathsToModuleNameMapper(
+    compilerOptions.paths,
+    { prefix: '<rootDir>/' }
+  ),
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  testMatch: [
+    '**/tests/**/*.test.(ts?(x)|js?(x))'
+  ],
+  verbose: true,
+};
