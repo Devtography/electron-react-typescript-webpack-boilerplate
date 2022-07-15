667955e9c06bff1cd580207f6e606d536dffbfd4
Wing Chau <wing@devtography.com>
2022-07-15 05:19:03 +0100

    Migrate to `createRoot`
    - Use new `createRoot` API from React v18.

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -6,9 +6,9 @@ import '_public/style.css';
 
 import App from '_renderer/App';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
-);
+const container = document.getElementById('app');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(<App />);
