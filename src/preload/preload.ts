e48ad97b17b7469e3e3dc49df65c866bb88d9ffc
Wing Chau <wing@devtography.com>
2022-08-10 04:30:43 +0100

    Fix cannot find module issue
    - Update local import with `.js` extension to comply with ES module rules on
    relative imports.

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { contextBridge } from 'electron';
-import ipcAPI from '_preload/ipc-api';
+import ipcAPI from '_preload/ipc-api.js';
 
 contextBridge.exposeInMainWorld('ipcAPI', ipcAPI);
