9e8fdb443402b65250041c44fd056653a6107517
Wing Chau <wing@devtography.com>
2022-08-15 21:30:49 +0100

    Rollback `moduleResolution` to `Node`
    - Rollbacked the `moduleResolution` setting to `Node` in `tsconfig`.
    - Removed file extension (`.js`) on relative imports.

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { contextBridge } from 'electron';
-import ipcAPI from '_preload/ipc-api.js';
+import ipcAPI from '_preload/ipc-api';
 
 contextBridge.exposeInMainWorld('ipcAPI', ipcAPI);
